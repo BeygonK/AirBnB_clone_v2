ce5168b6d940d6f88fe7a911ad12e43a886ea130
BeygonK <kipkoechbeygon@gmail.com>
2024-04-20 13:09:33 +0300

    set up for mysql server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant previleges to the user created to the database
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- GRANT SELECT privilege
-GRANT SELECT ON 'perfomance_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
