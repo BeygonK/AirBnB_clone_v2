026ea650b2e955c9b861225e39eab7d4d85658c2
BeygonK <kipkoechbeygone@gmail.com>
2024-05-28 11:56:23 +0300

    added docs

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This moddule starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
 from models import *
@@ -9,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display a HTML page with the states and cities"""
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
@@ -20,5 +22,4 @@ def teardown_db(exception):
     storage.close()
 
 if __name__ == '__main__':
-    """Runs the application"""
     app.run(host='0.0.0.0', port='5000')
