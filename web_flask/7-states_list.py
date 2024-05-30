64a12294870641ec9e48ad8636d975ddbd691821
BeygonK <kipkoechbeygone@gmail.com>
2024-05-30 08:06:20 +0300

    documented

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,6 +19,7 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
 if __name__ == '__main__':
     """Runs the application"""
     app.run(host='0.0.0.0', port='5000')
